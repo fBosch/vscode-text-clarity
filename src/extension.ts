c7b46fadd870930b7f022c46c3608a7a8f476a60
Frederik Bosch <frede.bosch@gmail.com>
2017-04-14 17:46:00 +0200

    fixed button text

--- src/extension.ts
+++ src/extension.ts
@@ -9,47 +9,63 @@ const appDir = path.dirname(require.main.filename)
 const base = appDir + (isWin ? '\\vs\\workbench' : '/vs/workbench');
 const htmlFile = base + (isWin ? '\\electron-browser\\bootstrap\\index.html' : '/electron-browser/bootstrap/index.html');
 
-export function activate(context: vscode.ExtensionContext) {
+function install(context) {
     try {
         let html = fs.readFileSync(htmlFile, 'utf-8')
-        const isEnabled = "JSON.parse(localStorage['storage://global/fbosch.text-clarity']).enabled"
-        const isNotDisabled = "!localStorage['storage://global/extensions/disabled'].includes('vscode-text-clarity')"
-        if (!html.includes(identifier)) {
-            html = html.replace('</body>', `
-                ${identifier}
-                <style type="text/css">
+        const isNotDisabled = "!localStorage['storage://global/extensions/disabled'].includes('fbosch.text-clarity')"
+        const isEnabled = "!!JSON.parse(localStorage['storage://global/fbosch.text-clarity']).enabled"
+        if (html.indexOf(identifier) === -1) {
+            html = html.replace('</body>', `${identifier}
+            <style type="text/css">
                     .text-clarity-enabled .editor-container, .text-clarity-enabled .part.panel .content {
                         -webkit-font-smoothing: none !important;
                     }
                 </style>
                 <script type="text/javascript">
-                    document.addEventListener("DOMContentLoaded", function(event) { 
-                        if (${isNotDisabled} && ${isEnabled}) {
-                            document.querySelector('body').classList.add('text-clarity-enabled')
-                        }
-                    })
+                    if (${isNotDisabled} && ${isEnabled}) {
+                        document.querySelector('body').classList.add('text-clarity-enabled')
+                    }
                 </script>
                 ${identifier}
             </body>`)
-            fs.writeFileSync(htmlFile, html, 'utf-8')
-            context.globalState.update('enabled', true)
+            fs.writeFile(htmlFile, html, 'utf-8', () => {
+                context.globalState.update('enabled', true)
+                vscode.window.showInformationMessage('Text Clarity has been successfully installed', { title: "Restart Visual Studio" })
+                    .then(function (msg) {
+                        vscode.commands.executeCommand("workbench.action.reloadWindow");
+                    })
+            })
         }
     } catch (e) {
         console.log(e)
     }
 }
 
-// this method is called when your extension is deactivated
-export function deactivate(context: vscode.ExtensionContext) {
+function uninstall() {
     try {
         let html = fs.readFileSync(htmlFile, 'utf-8')
-        if (html.includes(identifier)) {
-            let identifierRegex = new RegExp(`${identifier}([\s\S]*?)${identifier}`)
+        if (html.indexOf(identifier) !== -1) {
+            let identifierRegex = new RegExp(`${identifier}([\\s\\S]*?)${identifier}`)
             html = html.replace(identifierRegex, '')
-            fs.writeFileSync(htmlFile, html, 'utf-8')
-            context.globalState.update('enabled', false)            
+            fs.writeFile(htmlFile, html, 'utf-8', () => {
+                vscode.window.showInformationMessage('Text Clarity has been made ready for uninstallation')
+            })
         }
     } catch (e) {
         console.log(e)
     }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+    install(context)
+
+    vscode.commands.registerCommand('extension.installTextClarity', () => install(context))
+    vscode.commands.registerCommand('extension.uninstallTextClarity', uninstall)
+}
+
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+
 }
